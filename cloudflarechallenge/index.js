7751daa893089f385ad5e0729ad88860087afe84
Devansh Sharma <devansh@Devanshs-MacBook-Pro.local>
2020-04-16 22:51:43 -0700

    minor changes

--- cloudflarechallenge/index.js
+++ cloudflarechallenge/index.js
@@ -2,26 +2,47 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
+const data = { username: 'example' };
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-	return fetch('https://cfw-takehome.developers.workers.dev/api/variants')
-	.then(function (response) {
+	return fetch('https://cfw-takehome.developers.workers.dev/api/variants',{
+	  method: 'GET',
+	  headers: {
+	    'Content-Type': 'application/json',
+	  }
+	}).then(function (response) {
 		return response.json();
 	}).then(function (json) {
 
 		var arr = json.variants;
-		console.log(arr[1]);
+		// 50/50 split 
+		var url = Math.random() < 0.5 ? arr[1] : arr[0] 
 
-		//add logic to select random values
-
-		return fetch(arr[1])
+		return fetch(url)
 			.then(response => response.text())
 			.then(function (html) {
-				return new Response(html, {headers: { 'content-type': 'text/html' }})
+				if (url == "https://cfw-takehome.developers.workers.dev/variants/1") {
+					html = html.replace('Variant 1', "This is a new title")
+					html = html.replace('Variant 1', "Coding challenge completed.")
+					html = html.replace("This is variant one of the take home project!","Hacked Variation 1")
+					html = html.replace('href="https://cloudflare.com"', 'href="https://linkedin.com/in/dev204"')
+					html = html.replace('Return to cloudflare.com', "Open my Linkedin Profile!")
+					return new Response(html, {headers: { 'content-type': 'text/html' }});;
+				}
+				else {
+					html = html.replace(new RegExp('Variant 2'), "This is a new title")
+					html = html.replace('Variant 2', "Coding challenge completed.")
+					html = html.replace("This is variant two of the take home project!","Hacked Variation 2")
+					html = html.replace('href="https://cloudflare.com"', 'href="https://linkedin.com/in/dev204"')
+					html = html.replace('Return to cloudflare.com', "Open my Linkedin Profile!")
+					return new Response(html, {headers: { 'content-type': 'text/html' }});;
+				}
 			});
 	});
 
 }
+
